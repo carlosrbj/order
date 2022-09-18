a38b409e805716c5daac7e3145df918ecaec74d7
laranjazda <carloseng.jr@gmail.com>
2022-09-18 13:23:59 -0300

    add querydsl dependency

--- src/main/java/com/hsob/order/repository/OrderRepository.java
+++ src/main/java/com/hsob/order/repository/OrderRepository.java
@@ -5,9 +5,10 @@ import com.hsob.documentdb.order.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository <Order, Long>, QuerydslPredicateExecutor<Order> {
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("update Order p set p.status = :status where p = :order")
--- src/main/java/com/hsob/order/service/OrderService.java
+++ src/main/java/com/hsob/order/service/OrderService.java
@@ -1,16 +1,22 @@
 package com.hsob.order.service;
 
 import com.hsob.documentdb.order.Order;
+import com.hsob.documentdb.order.QOrder;
 import com.hsob.documentdb.order.Status;
+import com.hsob.documentdb.payment.Payment;
+import com.hsob.documentdb.payment.QPayment;
 import com.hsob.order.dto.order.OrderDto;
 import com.hsob.order.dto.order.StatusDto;
 import com.hsob.order.repository.OrderRepository;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +34,9 @@ public class OrderService {
     @Autowired
     private final ModelMapper modelMapper;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public List<OrderDto> getAll() {
         logger.log(Level.INFO, "Get list of Order start.");
         List<OrderDto> result = new ArrayList<>();
@@ -43,16 +52,23 @@ public class OrderService {
     }
 
     public OrderDto getOrderById(Long id) {
-        Order order;
+//        Order order;
+        Order result;
         try {
             logger.log(Level.INFO, "Get order for id: {$id} start.");
-            order = orderRepository.getOrderByItemId(id);
+            // order = orderRepository.getOrderByItemId(id);
+            QOrder order = QOrder.order;
+            JPAQuery<Payment> query = new JPAQuery<>(entityManager);
+            result = query.select(order)
+                    .from(order)
+                    .where(order.id.eq(id))
+                    .fetchOne();
             logger.log(Level.INFO, "Order found.");
         } catch (EntityNotFoundException e){
             logger.log(Level.WARNING, EntityNotFoundException.class.descriptorString());
            throw new RuntimeException(e.getCause());
         }
-        if (order != null) return modelMapper.map(order, OrderDto.class);
+        if (result != null) return modelMapper.map(result, OrderDto.class);
         logger.log(Level.INFO, "Order not found.");
         return null;
     }
